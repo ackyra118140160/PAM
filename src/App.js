c071040fec4bddc25869c7e697aa77589b46ee2c
ackyras <ackyrasibarani@gmail.com>
2021-03-09 22:12:10 +0700

    tugas redux minggu 4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import {useSelector, useDispatch} from 'react-redux';
+import {sign_in} from './actions';
+
 
 function App() {
+  const logged= useSelector(state=>state.loggedReducer)
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Status aktif atau tidak aktif</h1>
+      {logged ? <h2>anda sedang aktif</h2>:<h2>anda sedang tidak aktif</h2>}
+      <button onClick={()=>dispatch(sign_in())}>aktifkan</button>
     </div>
   );
 }
